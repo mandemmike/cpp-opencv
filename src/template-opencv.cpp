bfa601a0c11d177e02f60cb0d1417d941152af09
mandemmike <81154352+mandemmike@users.noreply.github.com>
2022-04-26 22:50:16 +0200

    Update template-opencv.cpp

--- src/template-opencv.cpp
+++ src/template-opencv.cpp
@@ -97,7 +97,7 @@ int32_t main(int32_t argc, char **argv) {
                     std:: string a = tm;
 
                     const auto Time = std::chrono::system_clock::now();
-                    std:: string b = std::to_string(std:chrono::duration_cast<std::chrono::microseconds>(
+                    std:: string b = std::to_string(std::chrono::duration_cast<std::chrono::microseconds>(
                         Time.time_since_epoch()).count());
                     std:: string s = "NOW : " + a + " ts ; " + b + " ; Araya, Michael";
                     
@@ -106,7 +106,7 @@ int32_t main(int32_t argc, char **argv) {
                 // Example: Draw a red rectangle and display image.
                 //cv::rectangle(img, cv::Point(50, 50), cv::Point(100, 100), cv::Scalar(0,0,255));
 
-                cv::putText(img, s, cv::Point(100,10), cv::FONT_HERSHEY_DUPLEX, 0,4, cv::Scalar(250,0,0));
+                cv::putText(img, s, cv::Point(100,10), cv::FONT_HERSHEY_DUPLEX, 0,4, cv::Scalar(0,0,250));
 
                 // If you want to access the latest received ground steering, don't forget to lock the mutex:
                 {
